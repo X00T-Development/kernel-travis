cf39a688e06d73799a82f9a3b4ec4a941b40f09d
ShanuDey <sd4shanudey@gmail.com>
2019-12-12 11:26:06 +0530

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+echo "Cloning dependencies"
+git clone --depth=1 -b lineage-16.0 https://github.com/X00T-Development/kernel_asus_sdm660 kernel
+cd kernel
+git clone --depth=1 https://github.com/Haseo97/Clang-10.0.0 clang
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r39 stock
+git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r39 stock_32
+git clone --depth=1 https://github.com/X00T-Development/AnyKernel3 -b master AnyKernel
+echo "Done"
+GCC="$(pwd)/aarch64-linux-android-"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/X00T_defconfig
+PATH="${PWD}/clang/bin:${PWD}/stock/bin:${PWD}/stock_32/bin:${PATH}"
+export ARCH=arm64
+export KBUILD_BUILD_HOST=Kernel
+export KBUILD_BUILD_USER="root"
+# Send info plox channel
+function sendinfo() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=html" \
+        -d text="<b>• kernel •</b>%0ABuild started on <code>travis CI/CD</code>%0AFor device <b>Asus Zenfone Max Pro M1 (X00T)</b>%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Test"
+}
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Asus Zenfone Max Pro M1 (X00T)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+# Compile plox
+function compile() {
+   make O=out ARCH=arm64 X00T_defconfig
+       make -j$(nproc --all) O=out \
+                             ARCH=arm64 \
+			     CROSS_COMPILE=aarch64-linux-android- \
+			     CROSS_COMPILE_ARM32=arm-linux-androideabi-
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Kernel-X00T-${TANGGAL}.zip *
+    cd .. 
+}
+sticker
+sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
